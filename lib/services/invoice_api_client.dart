7b73829d860bbf2cb3174e22867390c808c7693f
mustafa-altai77 <116103164+mustafa-altai77@users.noreply.github.com>
2023-07-09 17:30:38 +0400

    Update invoice_api_client.dart

--- lib/services/invoice_api_client.dart
+++ lib/services/invoice_api_client.dart
@@ -11,76 +11,9 @@ import 'package:path/path.dart';
 
 
 class InvoiceApiClient {
-  static const baseUrl = ApiConstants.BASE_URL+'/api';
-  static final userName = Prefer.prefs!.getString('userName');
-  final http.Client httpClient;
 
-  InvoiceApiClient({http.Client? httpClient})
-      : httpClient = httpClient ?? http.Client();
-
-
-  Future<InvoiceModel> createInvoice(
-{int? invoiceId,int? localityId,int? stationId,int? companyId
-,double? quantity,String? plateNuber,String? driverName,String? driverPhone,String? note,
-String? plateChar,int? vehicleID,int? fuelTyeId,int? gasAgentID
-}
-    ) async {
-    final url = '$baseUrl/invoice';
-
-    final token = Prefer.prefs!.getString('token');
-
-    final response = await this.httpClient.post(
-          url,
-          headers: requestHeaders(token),
-          body: jsonEncode(
-            {
-              'PlateNumber': plateNuber,
-              'PlateChar': plateChar,
-              'Note': note,
-              'VehicleID': vehicleID,
-              'GasAgentID': gasAgentID,
-              'FualTypeID': fuelTyeId,
-              'Quantitty': quantity,
-              'CompanyId': companyId,
-              'LocalityId': localityId,
-              'DriverName': driverName,
-              'DriverPhone': driverPhone,
-              'StationId': stationId,
-              'InvoiceId': invoiceId
-            },
-          ),
-        );
-      print("status : ${jsonDecode(response.body)}");
-    if (response.statusCode != 200) {
-      throw Exception('Error sending message');
-    }
-
-    return InvoiceModel.fromJson(jsonDecode(response.body));
-  }
-
-  Future<List<InvoiceModel>> fetchBooings() async {
-    final url = '$baseUrl/invoice';
-    print("ana");
-    final token = Prefer.prefs!.getString('token');
-
-    final response = await this.httpClient.get(
-          url,
-          headers: requestHeaders(token),
-        );
-      print(jsonDecode(response.body));
-
-    if (response.statusCode != 200) {
-      print(response.body);
-      throw Exception('Invalid Credentials');
-    }
-
-    final specialtiesJson = jsonDecode(response.body)['result'] as List;
-    return specialtiesJson
-        .map((specialties) => InvoiceModel.fromJson(specialties))
-        .toList();
   }
 
 
 
 
-}
